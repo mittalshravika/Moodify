6d6c9569863f7778f9bc03a084da57a1d01d64c6
Shravika Mittal <shravika16093@iiitd.ac.in>
2017-08-19 03:02:46 +0530

    MoodPage updated

--- app/src/main/java/com/example/sm_06/moodify/MoodPage.java
+++ app/src/main/java/com/example/sm_06/moodify/MoodPage.java
@@ -1,7 +1,9 @@
 package com.example.sm_06.moodify;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 /**
@@ -15,6 +17,54 @@ public class MoodPage extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mood_page);
 
+        ImageView happy = (ImageView)findViewById(R.id.happyMood);
+        ImageView sad = (ImageView)findViewById(R.id.sadMood);
+        ImageView classics = (ImageView)findViewById(R.id.classicsMood);
+        ImageView inspirational = (ImageView)findViewById(R.id.inspirationalMood);
+        ImageView romantic = (ImageView)findViewById(R.id.romanticMood);
+        ImageView trap = (ImageView)findViewById(R.id.trapMood);
+
+        happy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        sad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        classics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        inspirational.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        romantic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        trap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
