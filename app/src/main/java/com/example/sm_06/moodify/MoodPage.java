34ce63d614f5c9b433e7a360bd7b991d0a03f9d0
Meghna Gupta <meghnagupta64@gmail.com>
2017-08-19 01:42:49 +0530

    Login and Signup system added

---
+++ app/src/main/java/com/example/sm_06/moodify/MoodPage.java
@@ -0,0 +1,17 @@
+package com.example.sm_06.moodify;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HP on 19-08-2017.
+ */
+
+public class MoodPage extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
