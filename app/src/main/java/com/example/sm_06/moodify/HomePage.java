04cc7bbbf1706e1055ebeb6963c20e5791d09830
Meghna Gupta <meghnagupta64@gmail.com>
2017-08-19 00:51:57 +0530

    Some update

---
+++ app/src/main/java/com/example/sm_06/moodify/HomePage.java
@@ -0,0 +1,16 @@
+package com.example.sm_06.moodify;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HP on 19-08-2017.
+ */
+
+public class HomePage extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
--- app/src/main/java/com/example/sm_06/moodify/MainActivity.java
+++ app/src/main/java/com/example/sm_06/moodify/MainActivity.java
@@ -1,13 +1,100 @@
 package com.example.sm_06.moodify;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected EditText emailEditText;
+    protected EditText passwordEditText;
+    protected Button logInButton;
+    protected TextView signUpTextView;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        signUpTextView = (TextView) findViewById(R.id.signup);
+        emailEditText = (EditText) findViewById(R.id.username);
+        passwordEditText = (EditText) findViewById(R.id.password);
+        logInButton = (Button) findViewById(R.id.login);
+        CheckBox chk = (CheckBox)findViewById(R.id.checkBox);
+
+        chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    passwordEditText.setInputType(129);
+                }
+            }
+        });
+
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                email = email.trim();
+                password = password.trim();
+                if (email.isEmpty() || password.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Hello")
+                            .setTitle("Hello")
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(MainActivity.this, HomePage.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    } else {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                        builder.setMessage(task.getException().getMessage())
+                                                .setTitle("Hello")
+                                                .setPositiveButton(android.R.string.ok, null);
+                                        AlertDialog dialog = builder.create();
+                                        dialog.show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
 }
